a07faa8be8c0bae20227256d39a27f452049c7ab
GReddyD <greddy553@yandex.ru>
2016-08-29 16:22:52 +0300

    Update 8ball.rb

--- 8ball.rb
+++ 8ball.rb
@@ -1,15 +1,15 @@
 # encoding: UTF-8
 
-require_relative "answers_ballball.rb"
-require_relative "words_ballball.rb"
+require_relative "answers_ball.rb"
+require_relative "words_ball.rb"
 
 # Случайный выбор фразы из массива words_text
 wordsball = WordsBall.new
-puts wordsball.get_wordsball
+puts wordsball.words_ball
 
 # Добавили загадочности между двумя фразами, ожидание 5 секунд.
 sleep 5
 
 # Случайный выбор фразы из массива answers
 answerball = AnswersBall.new
-puts answerball.get_answerball
+puts answerball.answer_ball
